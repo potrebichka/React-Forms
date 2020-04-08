c0a7cb095120cf486730e9e001d751a4893187b0
Nina Potrebich <potrebich@gmail.com>
2020-04-07 22:06:36 -0700

    Add propsand handle methods to Input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,10 +5,10 @@ const Input = (props) => {
         <div className="form-group">
             <label htmlFor={props.name} className="form-label">{props.title}</label>
             <input 
-                className="form-input"
+                className="form-control"
                 id={props.name}
                 name={props.name}
-                type={props.name}
+                type={props.type}
                 value={props.value}
                 onChange={props.handleChange}
                 placeholder={props.placeholder}
--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 
 // Import Components
-import CheckBox from '../components/CheckBox';
+//import CheckBox from '../components/CheckBox';
 import Input from '../components/Input';
-import TextArea from '../components/TextArea';
-import Select from '../components/Select';
-import Button from '../components/Button';
+// import TextArea from '../components/TextArea';
+// import Select from '../components/Select';
+// import Button from '../components/Button';
 
 class FormContainer extends Component {
     constructor(props) {
@@ -34,16 +34,56 @@ class FormContainer extends Component {
         // Logic for resetting the form
     }
 
+    // handleFullName = (e) => {
+    //     let value = e.target.value;
+    //     this.setState(prevState => ({newUser: {...prevState.newUser, name: value}}));
+    // }
+
+    // handleAge = (e) => {
+    //     let value = e.target.value;
+    //     this.setState(prevState => ({newUser: {...prevState.newUser, age: value}}), () => console.log(this.state.newUser))
+    // }
+
+    handleFullName = (e) => {
+        let value = e.target.value;
+        let name = e.target.name;
+        this.setState(
+            prevState => {
+                return {
+                    newUser: {
+                        ...prevState.newUser, 
+                        [name]: value
+                    }
+                }
+            }, () => console.log(this.state.newUser)
+        );
+    }
+
     render() {
         return (
             <form className="container" onSubmit={this.handleFormSubmit}>
-                <Input /> {/* Name of the user */}
-                <Input /> {/* Input for Age */}
-                <Select /> {/* Gender Selection */}
-                <CheckBox /> {/* List of Skills */}
-                <TextArea /> {/* About you */}
-                <Button /> {/* Submit */}
-                <Button /> {/* Clear the form */}
+                <Input 
+                    type="text"
+                    name="name"
+                    title="Full Name"
+                    value={this.state.newUser.name}
+                    placeholder="Enter your name"
+                    handleChange={this.handleInput}
+                /> 
+                <Input 
+                    type="number"
+                    name="age"
+                    title="Age"
+                    value={this.state.newUser.age}
+                    placeholder="Enter your age"
+                    handleChange={this.handleInput}
+                /> 
+                {/* <Select /> 
+                <CheckBox />
+                <TextArea />
+                <Button /> 
+                <Button /> */}
+
             </form>
         );
     }
